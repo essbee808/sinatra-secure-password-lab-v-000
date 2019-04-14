5f808d874461c122c42c9f3a5a76a452c59a8145
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-14 18:24:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username] == nil || params[:password] == nil
+    if params[:username] == "" || params[:password] == ""
     user = User.new(:username => params[:username], :password => params[:password])
     
     if user.save
