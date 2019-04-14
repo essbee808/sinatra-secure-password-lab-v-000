a621ca572d63f0192ace7e4f083147f243fe2d45
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-14 18:49:01 +0000

    Automatically backed up by Learn

--- db/migrate/20190414180506_users.rb
+++ db/migrate/20190414180506_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration[5.1]
   def up
     create_table :users do |t|
       t.string :username
-      t.string :password_digest
+      t.string :username
     end
   def down
     drop_table :users
