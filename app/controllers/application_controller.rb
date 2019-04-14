d5ef1ab134e7ada0b3c8c0e7b89dba60d54723d5
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-14 18:27:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    binding.pry
+    #binding.pry
     if params[:username] == "" || params[:password] == ""
       redirect "/failure"
     else
