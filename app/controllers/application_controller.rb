9cd5ece779abde137a6d20d11279065566a7b323
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-14 18:48:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == "" || params[:password] == ""
       redirect "/failure"
     else
-      binding.pry
+      #binding.pry
       user = User.new(:username => params[:username], :password => params[:password])
       user.save
       redirect "/login"
